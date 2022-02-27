c132ef3bb58555f3f4fdb5317b158642fff38e37
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-02-27 00:17:58 -1100

    Add base functionality

--- script.js
+++ script.js
@@ -1,63 +1,100 @@
-let retVals = ["Rock","Paper","Scissors"];
+let rps = ["Rock", "Paper", "Scissors"];
+
 function computerPlay(){
     let choice = Math.floor(Math.random()*3);  // index between 0 and 2 inclusive
-    return retVals[choice]; //returns either R or P or S
+    return choice; //returns either 0=R or 1=P or 2=S
 }
 
 function playRound(playerSelection, computerSelection){
-    switch(playerSelection.toLowerCase()){
-        case "rock":
-            switch(computerSelection.toLowerCase()){
-                case "rock":
-                    return "Tie";
-                case "paper":
-                    return "You Lose! Paper beats Rock.";
-                case "scissors":
-                    return "You Win! Rock beats Scissors";
+    let roundResult = 0;
+    switch(playerSelection){
+        case 0:
+            switch(computerSelection){
+                case 0:
+                    roundResult = 0; //0 for tie
+                    break;
+                case 1:
+                    roundResult = -1; //-1 for loss
+                    break;
+                case 2:
+                    roundResult = 1; //+1 for win
+                    break;
             }
-        case "paper":
-            switch(computerSelection.toLowerCase()){
-                case "rock":
-                    return "You Win! Paper beats Rock.";
-                case "paper":
-                    return "Tie";
-                case "scissors":
-                    return "You Lose! Scissors beat Paper.";
+            break;
+        case 1:
+            switch(computerSelection){
+                case 0:
+                    roundResult = 1;
+                    break;
+                case 1:
+                    roundResult = 0;
+                    break;
+                case 2:
+                    roundResult = -1;
+                    break;
             }
-        case "scissors":
-            switch(computerSelection.toLowerCase()){
-                case "rock":
-                    return "You Lose! Rock beats Scissors";
-                case "paper":
-                    return "You Win! Scissors beat Paper.";
-                case "scissors":
-                    return "Tie";
+            break;
+        case 2:
+            switch(computerSelection){
+                case 0:
+                    roundResult = -1;
+                    break;
+                case 1:
+                    roundResult = 1;
+                    break;
+                case 2:
+                    roundResult = 0;
+                    break;
             }
+            break;
+    }
+    
+    switch(roundResult){
+        case -1:
+            resultElem.textContent = `You Lose! ${rps[computerSelection]} beats ${rps[playerSelection]}.`;
+            computerScore += 1;
+            break;
+        case 0:
+            resultElem.textContent = `Tie!`;
+            break;
+        case 1:
+            resultElem.textContent = `You Win! ${rps[playerSelection]} beats ${rps[computerSelection]}.`;
+            playerScore += 1;
+            break;
+    }
+
+    scoreElem.textContent = `Player ${playerScore}:${computerScore} Computer`;
+    if(playerScore === 5 && computerScore < 5){
+        resultElem.textContent = `Player Wins!`;
+        playerScore = 0;
+        computerScore = 0;
+    }else if(computerScore === 5 && playerScore < 5){
+        resultElem.textContent = `Computer Wins!`;
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, Paper or Scissors ?");
-        switch(playRound(playerSelection, computerPlay()).slice(0, 8)){
-            case "You Win!":
-                playerScore++;
-                break;
-            case "You Lose":
-                computerScore++;
-                break;
-            default:
-                break;
-        }
-    }
 
-    if(playerScore > computerScore){
-        console.log(`You Win! ${playerScore}:${computerScore}`);
-    }else if(playerScore < computerScore){
-        console.log(`You Lose! ${playerScore}:${computerScore}`);
-    }else{
-        console.log(`Tie! ${playerScore}:${computerScore}`);
-    }
-}
+let rockB = document.querySelector("#rock");
+let paperB = document.querySelector("#paper");
+let scissorsB = document.querySelector("#scissors");
+
+let resultElem = document.querySelector(".result");
+let scoreElem = document.querySelector(".score");
+
+let playerScore = 0;
+let computerScore = 0;
+
+rockB.addEventListener("click", ()=>{
+    let cpChoice = computerPlay();
+    playRound(0, cpChoice);
+});
+paperB.addEventListener("click", ()=>{
+    let cpChoice = computerPlay();
+    playRound(1, cpChoice);
+});
+scissorsB.addEventListener("click", ()=>{
+    let cpChoice = computerPlay();
+    playRound(2, cpChoice);
+});
